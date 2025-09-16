26150d970e749ebcbea74aea1413e82636437d6f
jhonbslv <jhonbslv@gmail.com>
2025-09-16 07:53:13 -0300

    ortografiacorrigir

--- app.py
+++ app.py
@@ -9,6 +9,27 @@ def main():
         try:
             opcao = int(input("escolha uma opção: "))
         except ValueError:
-            print("opção inválida, digite um número.")
+            print("opção inválida, digite um número")
             continue
+
+        match opcao:
+            case 1:
+                cadastrar_cliente(locadora)
+            case 2:
+                cadastrar_filme(locadora)
+            case 3:
+                cadastrar_jogo(locadora)
+            case 4:
+                locadora.listar_clientes()
+            case 5:
+                locadora.listar_itens()
+            case 6:
+                locar_item(locadora)
+            case 7:
+                devolver_item(locadora)
+            case 0:
+                print("saindo do sistema")
+                break
+            case _:
+                print("opção inválida")
 
